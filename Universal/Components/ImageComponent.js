c8e121df73d7510f6ac44923975c4495f94fd77c
naveenios21 <sfs.naveen21@gmail.com>
2023-01-04 22:53:13 +0530

    Add Image component

---
+++ Universal/Components/ImageComponent.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native'
+import React from 'react'
+import CharacterPNG from '../src/character.png'
+import CharacterSVG from '../src/character.svg'
+
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Image source={CharacterPNG} />
+      <Image source={require('../src/character.png')} style={{ width: 100, height: 100 }} />
+
+      <Image source={{ uri: 'https://reactjs.org/logo-og.png' }}
+        style={{ width: 100, height: 100, resizeMode: 'contain' }} />
+
+      <ImageBackground source={Char}
+        style={{ width: 100, height: 100, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, color: '#000' }}>Inside</Text>
+      </ImageBackground>
+
+    </View>
+  )
+}
+
+export default FloatingButton
+
+const styles = StyleSheet.create({})
